898a6af53686a83c773115745835946281494e5b
Andrew <vandyand@gmail.com>
2020-02-12 17:00:05 -0500

    updated dynamic coloring function and scrollbar

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 let slider = document.getElementById("myRange");
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
+updateColor = () => {
     // let color = 'red';
     // let color = '#'+String(slider.value).padStart(3,'0');
     // let color = 'rgb(100,100,100)';
-    let color1 = 'hsl('+String(slider.value*3.6)+',50%,50%)';
-    let color2 = 'hsl('+String((slider.value*3.6-45)%360)+',50%,50%)';
-    let color3 = 'hsl('+String((slider.value*3.6+45)%360)+',50%,50%)';
+    let color1 = 'hsl('+String(slider.value*0.36)+',50%,25%)';
+    let color2 = 'hsl('+String(slider.value*0.36)+',50%,85%)';
+    let color3 = 'hsl('+String((slider.value*0.36+240)%360)+',50%,55%)';
     console.log(color1,color2,color3);
     document.body.style.backgroundColor = color1;
     for (let item of document.getElementsByClassName('card')){
         item.style.backgroundColor = color2;
         item.style.borderColor = color3;
     }
-    // document.getElementsByClassName('card').style.backgroundColor = color2;
-    // document.getElementsByClassName('card').style.borderColor = color3;
+    for (let item of document.getElementsByClassName('slider')){
+        item.style.backgroundColor = color2;
+        item.style.borderColor = color3;
+    }
+
+    var style = document.querySelector('[data="test"]');
+    style.innerHTML = ".slider::-webkit-slider-thumb { background-color: " + color3 + " !important; }";
 }
+
+document.onload = updateColor();
+slider.oninput = () => updateColor();
